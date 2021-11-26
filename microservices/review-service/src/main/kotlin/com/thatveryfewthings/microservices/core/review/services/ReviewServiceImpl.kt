547088f3950e9643ab0d7892a4a183b982038c6d
Hauke Stange <hauke.stange@jimdo.com>
2021-11-26 12:34:47 +0100

    Integrate persistence into review service layer

--- microservices/review-service/src/main/kotlin/com/thatveryfewthings/microservices/core/review/services/ReviewServiceImpl.kt
+++ microservices/review-service/src/main/kotlin/com/thatveryfewthings/microservices/core/review/services/ReviewServiceImpl.kt
@@ -4,33 +4,56 @@ import com.thatveryfewthings.api.core.review.Review
 import com.thatveryfewthings.api.core.review.ReviewService
 import com.thatveryfewthings.api.exceptions.InvalidInputException
 import com.thatveryfewthings.api.http.ServiceUtil
+import com.thatveryfewthings.microservices.core.review.persistence.ReviewRepository
 import org.slf4j.LoggerFactory
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ReviewServiceImpl(
-    private val serviceUtil: ServiceUtil
+    private val repository: ReviewRepository,
+    private val mapper: ReviewMapper,
+    private val serviceUtil: ServiceUtil,
 ) : ReviewService {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
     override fun getReviews(productId: Int): List<Review> {
-
         if (productId < 1) {
             throw InvalidInputException("Invalid productId: $productId")
         }
 
-        if (productId == 213) {
-            log.debug("No reviews found for productId: $productId")
-            return emptyList()
+        val reviewEntities = repository.findByProductId(productId)
+        val reviews = mapper.entityListToApiList(reviewEntities).map {
+            it.addServiceAddress()
         }
 
-        return listOf(
-            Review(productId, 1, "Author 1", "Subject 1", "Content 1", serviceUtil.serviceAddress),
-            Review(productId, 2, "Author 2", "Subject 2", "Content 2", serviceUtil.serviceAddress),
-            Review(productId, 3, "Author 3", "Subject 3", "Content 3", serviceUtil.serviceAddress),
-        ).also {
-            log.debug("/reviews response size: ${it.size}")
+        return reviews.also {
+            log.debug("getReviews: response size: ${it.size}")
         }
     }
+
+    override fun createReview(review: Review): Review {
+        return try {
+            val reviewEntity = mapper.apiToEntity(review)
+            val newReview = repository.save(reviewEntity)
+
+            log.debug("createReview: created a review entity: ${review.productId}/${review.reviewId}")
+
+            mapper.entityToApi(newReview).addServiceAddress()
+        } catch (ex: DataIntegrityViolationException) {
+            throw InvalidInputException("Duplicate key, Product id: ${review.productId}, Review id: ${review.reviewId}")
+        }
+    }
+
+    override fun deleteReviews(productId: Int) {
+        log.debug("deleteReviews: tries to delete reviews for the product with productId: $productId")
+        repository.findByProductId(productId).let {
+            repository.deleteAll(it)
+        }
+    }
+
+    private fun Review.addServiceAddress() = copy(
+        serviceAddress = serviceUtil.serviceAddress
+    )
 }
--- microservices/review-service/src/test/kotlin/com/thatveryfewthings/microservices/core/review/ReviewServiceApplicationTests.kt
+++ microservices/review-service/src/test/kotlin/com/thatveryfewthings/microservices/core/review/ReviewServiceApplicationTests.kt
@@ -1,92 +1,217 @@
 package com.thatveryfewthings.microservices.core.review
 
+import com.thatveryfewthings.api.core.product.Product
+import com.thatveryfewthings.api.core.review.Review
+import com.thatveryfewthings.microservices.core.review.persistence.ReviewRepository
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.http.HttpStatus.BAD_REQUEST
-import org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.kotlin.core.publisher.toMono
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(
+    webEnvironment = RANDOM_PORT,
+    properties = ["spring.datasource.url=jdbc:h2:mem:review-db"],
+)
 class ReviewServiceApplicationTests(
     @Autowired
-    private val client: WebTestClient
+    private val client: WebTestClient,
+    @Autowired
+    private val repository: ReviewRepository,
 ) {
 
+    @BeforeEach
+    fun clearDb() {
+        repository.deleteAll()
+    }
+
     @Test
-    fun getReviewsByProductId() {
-
+    fun getReviewsByProduct() {
+        // Given
         val productId = 1
+        val reviewIds = listOf(1, 2, 3)
 
-        client.get()
-            .uri("/review?productId=$productId")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isOk
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.length()").isEqualTo(3)
-            .jsonPath("$[0].productId").isEqualTo(productId)
+        assertEquals(0, repository.findByProductId(productId).size)
+
+        // When
+        reviewIds.forEach {
+            postAndVerifyReview(productId, it, HttpStatus.OK)
+        }
+
+        // Then
+        assertEquals(reviewIds.size, repository.findByProductId(productId).size)
+        getAndVerifyReviews(productId, HttpStatus.OK) {
+            jsonPath("$.length()").isEqualTo(reviewIds.size)
+
+            reviewIds.forEachIndexed { index, id ->
+                jsonPath("$[$index].productId").isEqualTo(productId)
+                jsonPath("$[$index].reviewId").isEqualTo(id)
+            }
+        }
+    }
+
+    @Test
+    fun duplicateError() {
+        // Given
+        val productId = 1
+        val reviewId = 1
+        assertEquals(0, repository.count())
+
+        // When
+        postAndVerifyReview(productId, reviewId, HttpStatus.OK) {
+            jsonPath("$.productId").isEqualTo(productId)
+            jsonPath("$.reviewId").isEqualTo(reviewId)
+        }
+
+        // Then
+        assertEquals(1, repository.count())
+        postAndVerifyReview(productId, reviewId, HttpStatus.UNPROCESSABLE_ENTITY) {
+            jsonPath("$.path").isEqualTo("/review")
+            jsonPath("$.message").isEqualTo("Duplicate key, Product id: $productId, Review id: $reviewId")
+        }
+        assertEquals(1, repository.count())
+    }
+
+    @Test
+    fun deleteReviews() {
+        // Given
+        val productId = 1
+        val recommendationId = 1
+
+        postAndVerifyReview(productId, recommendationId, HttpStatus.OK)
+        assertEquals(1, repository.findByProductId(productId).size)
+
+        // When
+        deleteAndVerifyReviews(productId, HttpStatus.OK)
+
+        // Then
+        assertEquals(0, repository.findByProductId(productId).size)
+        deleteAndVerifyReviews(productId, HttpStatus.OK)
     }
 
     @Test
     fun getReviewsMissingParameter() {
+        // Given
+        val missingProductIdQuery = ""
 
-        client.get()
-            .uri("/review")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isEqualTo(BAD_REQUEST)
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.path").isEqualTo("/review")
-            .jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present")
+        // When
+        getAndVerifyReviews(missingProductIdQuery, HttpStatus.BAD_REQUEST) {
+
+            // Then
+            jsonPath("$.path").isEqualTo("/review")
+            jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present")
+        }
     }
 
     @Test
     fun getReviewsInvalidParameter() {
+        // Given
+        val invalidProductIdQuery = "?productId=no-integer"
 
-        client.get()
-            .uri("/review?productId=no-integer")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isEqualTo(BAD_REQUEST)
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.path").isEqualTo("/review")
-            .jsonPath("$.message").isEqualTo("Type mismatch.")
+        // When
+        getAndVerifyReviews(invalidProductIdQuery, HttpStatus.BAD_REQUEST) {
+
+            // Then
+            jsonPath("$.path").isEqualTo("/review")
+            jsonPath("$.message").isEqualTo("Type mismatch.")
+        }
     }
 
     @Test
     fun getReviewsNotFound() {
+        // Given
+        val productIdWithNonExistentReviews = 213
 
-        val productIdNotFound = 213
+        // When
+        getAndVerifyReviews(productIdWithNonExistentReviews, HttpStatus.OK) {
 
-        client.get()
-            .uri("/review?productId=$productIdNotFound")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isOk
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.length()").isEqualTo(0)
+            // Then
+            jsonPath("$.length()").isEqualTo(0)
+        }
     }
 
     @Test
     fun getReviewsInvalidParameterNegativeValue() {
+        // Given
+        val invalidProductId = -1
 
-        val productIdInvalid = -1
+        // When
+        getAndVerifyReviews(invalidProductId, HttpStatus.UNPROCESSABLE_ENTITY) {
 
-        client.get()
-            .uri("/review?productId=$productIdInvalid")
+            // Then
+            jsonPath("$.path").isEqualTo("/review")
+            jsonPath("$.message").isEqualTo("Invalid productId: $invalidProductId")
+        }
+    }
+
+    private fun getAndVerifyReviews(
+        productId: Int,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit = {},
+    ) {
+        getAndVerifyReviews("?productId=$productId", expectedStatus, bodyAssertions)
+    }
+
+    private fun getAndVerifyReviews(
+        query: String,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit,
+    ) {
+        val bodyContentSpec = client.get()
+            .uri("/review$query")
             .accept(APPLICATION_JSON)
             .exchange()
-            .expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+            .expectStatus().isEqualTo(expectedStatus)
             .expectHeader().contentType(APPLICATION_JSON)
             .expectBody()
-            .jsonPath("$.path").isEqualTo("/review")
-            .jsonPath("$.message").isEqualTo("Invalid productId: $productIdInvalid")
+
+        bodyAssertions(bodyContentSpec)
+    }
+
+    private fun postAndVerifyReview(
+        productId: Int,
+        reviewId: Int,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit = {},
+    ) {
+        val review = Review(
+            productId = productId,
+            reviewId = reviewId,
+            author = "Author $reviewId",
+            subject = "Subject $reviewId",
+            content = "Content $reviewId",
+            serviceAddress = "some service address",
+        )
+
+        val bodyContentSpec = client.post()
+            .uri("/review")
+            .body(review.toMono(), Product::class.java)
+            .accept(APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isEqualTo(expectedStatus)
+            .expectHeader().contentType(APPLICATION_JSON)
+            .expectBody()
+
+        bodyAssertions(bodyContentSpec)
+    }
+
+    private fun deleteAndVerifyReviews(
+        productId: Int,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit = {}
+    ) {
+        val bodyContentSpec = client.delete()
+            .uri("/review?productId=$productId")
+            .accept(APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isEqualTo(expectedStatus)
+            .expectBody()
+
+        bodyAssertions(bodyContentSpec)
     }
 }
