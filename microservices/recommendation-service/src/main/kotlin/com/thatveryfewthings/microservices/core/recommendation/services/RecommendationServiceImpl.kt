88444757b0593b7c9cb0fce1368fe238f4531e12
Hauke Stange <hauke.stange@jimdo.com>
2021-11-26 11:41:51 +0100

    Integrate persistence into recommendation service layer

--- microservices/recommendation-service/src/main/kotlin/com/thatveryfewthings/microservices/core/recommendation/services/RecommendationServiceImpl.kt
+++ microservices/recommendation-service/src/main/kotlin/com/thatveryfewthings/microservices/core/recommendation/services/RecommendationServiceImpl.kt
@@ -4,33 +4,56 @@ import com.thatveryfewthings.api.core.recommendation.Recommendation
 import com.thatveryfewthings.api.core.recommendation.RecommendationService
 import com.thatveryfewthings.api.exceptions.InvalidInputException
 import com.thatveryfewthings.api.http.ServiceUtil
+import com.thatveryfewthings.microservices.core.recommendation.persistence.RecommendationRepository
 import org.slf4j.LoggerFactory
+import org.springframework.dao.DuplicateKeyException
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RecommendationServiceImpl(
-    private val serviceUtil: ServiceUtil
+    private val repository: RecommendationRepository,
+    private val mapper: RecommendationMapper,
+    private val serviceUtil: ServiceUtil,
 ) : RecommendationService {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
     override fun getRecommendations(productId: Int): List<Recommendation> {
-
         if (productId < 1) {
             throw InvalidInputException("Invalid productId: $productId")
         }
 
-        if (productId == 113) {
-            log.debug("No recommendations found for productId: $productId")
-            return emptyList()
+        val recommendationEntities = repository.findByProductId(productId)
+        val recommendations = mapper.entityListToApiList(recommendationEntities).map {
+            it.copy(
+                serviceAddress = serviceUtil.serviceAddress
+            )
         }
 
-        return listOf(
-            Recommendation(productId, 1, "Author 1", 1, "Content 1", serviceUtil.serviceAddress),
-            Recommendation(productId, 2, "Author 2", 2, "Content 2", serviceUtil.serviceAddress),
-            Recommendation(productId, 3, "Author 3", 3, "Content 3", serviceUtil.serviceAddress),
-        ).also {
-            log.debug("/recommendation response size: ${it.size}")
+        return recommendations.also {
+            log.debug("getRecommendation: response size: ${it.size}")
+        }
+    }
+
+    override fun createRecommendation(recommendation: Recommendation): Recommendation {
+        return try {
+            val recommendationEntity = mapper.apiToEntity(recommendation)
+            val newRecommendation = repository.save(recommendationEntity)
+
+            log.debug("createRecommendation: created a recommendation entity: ${recommendation.productId}/${recommendation.recommendationId}")
+
+            mapper.entityToApi(newRecommendation).copy(
+                serviceAddress = serviceUtil.serviceAddress
+            )
+        } catch (ex: DuplicateKeyException) {
+            throw InvalidInputException("Duplicate key, Product id: ${recommendation.productId}, Recommendation id: ${recommendation.recommendationId}")
+        }
+    }
+
+    override fun deleteRecommendations(productId: Int) {
+        log.debug("deleteRecommendations: tries to delete recommendations for the product with the productId: $productId")
+        repository.findByProductId(productId).let {
+            repository.deleteAll(it)
         }
     }
 }
--- microservices/recommendation-service/src/test/kotlin/com/thatveryfewthings/microservices/core/recommendation/RecommendationServiceApplicationTests.kt
+++ microservices/recommendation-service/src/test/kotlin/com/thatveryfewthings/microservices/core/recommendation/RecommendationServiceApplicationTests.kt
@@ -1,93 +1,217 @@
 package com.thatveryfewthings.microservices.core.recommendation
 
+import com.thatveryfewthings.api.core.product.Product
+import com.thatveryfewthings.api.core.recommendation.Recommendation
+import com.thatveryfewthings.microservices.core.recommendation.persistence.RecommendationRepository
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.http.HttpStatus.BAD_REQUEST
-import org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.kotlin.core.publisher.toMono
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(
+    webEnvironment = RANDOM_PORT,
+    properties = ["spring.data.mongodb.port: 0"],
+)
 class RecommendationServiceApplicationTests(
     @Autowired
-    private val client: WebTestClient
+    private val client: WebTestClient,
+    @Autowired
+    private val repository: RecommendationRepository,
 ) {
+
+    @BeforeEach
+    fun clearDb() {
+        repository.deleteAll()
+    }
 
     @Test
     fun getRecommendationsByProductId() {
-
+        // Given
         val productId = 1
+        val recommendationIds = listOf(1, 2, 3)
+        assertEquals(0, repository.findByProductId(productId).size)
 
-        client.get()
-            .uri("/recommendation?productId=$productId")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isOk
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.length()").isEqualTo(3)
-            .jsonPath("$[0].productId").isEqualTo(productId)
+        // When
+        recommendationIds.forEach {
+            postAndVerifyRecommendation(productId, it, HttpStatus.OK)
+        }
+
+        // Then
+        assertEquals(recommendationIds.size, repository.findByProductId(productId).size)
+        getAndVerifyRecommendations(productId, HttpStatus.OK) {
+            jsonPath("$.length()").isEqualTo(recommendationIds.size)
+
+            recommendationIds.forEachIndexed { index, id ->
+                jsonPath("$[$index].productId").isEqualTo(productId)
+                jsonPath("$[$index].recommendationId").isEqualTo(id)
+
+            }
+        }
+    }
+
+    @Test
+    fun duplicateError() {
+        // Given
+        val productId = 1
+        val recommendationId = 1
+        assertEquals(0, repository.count())
+
+        // When
+        postAndVerifyRecommendation(productId, recommendationId, HttpStatus.OK) {
+            jsonPath("$.productId").isEqualTo(productId)
+            jsonPath("$.recommendationId").isEqualTo(recommendationId)
+        }
+
+        // Then
+        assertEquals(1, repository.count())
+        postAndVerifyRecommendation(productId, recommendationId, HttpStatus.UNPROCESSABLE_ENTITY) {
+            jsonPath("$.path").isEqualTo("/recommendation")
+            jsonPath("$.message").isEqualTo("Duplicate key, Product id: $productId, Recommendation id: $recommendationId")
+        }
+        assertEquals(1, repository.count())
+    }
+
+    @Test
+    fun deleteRecommendations() {
+        // Given
+        val productId = 1
+        val recommendationId = 1
+
+        postAndVerifyRecommendation(productId, recommendationId, HttpStatus.OK)
+        assertEquals(1, repository.findByProductId(productId).size)
+
+        // When
+        deleteAndVerifyRecommendations(productId, HttpStatus.OK)
+
+        // Then
+        assertEquals(0, repository.findByProductId(productId).size)
+        deleteAndVerifyRecommendations(productId, HttpStatus.OK)
     }
 
     @Test
     fun getRecommendationsMissingParameter() {
+        // Given
+        val missingProductIdQuery = ""
 
-        client.get()
-            .uri("/recommendation")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isEqualTo(BAD_REQUEST)
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.path").isEqualTo("/recommendation")
-            .jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present")
+        // When
+        getAndVerifyRecommendations(missingProductIdQuery, HttpStatus.BAD_REQUEST) {
+
+            // Then
+            jsonPath("$.path").isEqualTo("/recommendation")
+            jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present")
+        }
     }
 
     @Test
     fun getRecommendationsInvalidParameter() {
+        // Given
+        val invalidProductIdQuery = "?productId=no-integer"
 
-        client.get()
-            .uri("/recommendation?productId=no-integer")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isEqualTo(BAD_REQUEST)
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.path").isEqualTo("/recommendation")
-            .jsonPath("$.message").isEqualTo("Type mismatch.")
+        // When
+        getAndVerifyRecommendations(invalidProductIdQuery, HttpStatus.BAD_REQUEST) {
+
+            // Then
+            jsonPath("$.path").isEqualTo("/recommendation")
+            jsonPath("$.message").isEqualTo("Type mismatch.")
+        }
     }
 
     @Test
     fun getRecommendationsNotFound() {
+        // Given
+        val productIdWithNonExistentRecommendations = 113
 
-        val productIdNotFound = 113
+        // When
+        getAndVerifyRecommendations(productIdWithNonExistentRecommendations, HttpStatus.OK) {
 
-        client.get()
-            .uri("/recommendation?productId=$productIdNotFound")
-            .accept(APPLICATION_JSON)
-            .exchange()
-            .expectStatus().isOk
-            .expectHeader().contentType(APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.length()").isEqualTo(0)
-
+            // Then
+            jsonPath("$.length()").isEqualTo(0)
+        }
     }
 
     @Test
     fun getRecommendationsInvalidParameterNegativeValue() {
+        // Given
+        val invalidProductId = -1
 
-        val productIdInvalid = -1
+        // When
+        getAndVerifyRecommendations(invalidProductId, HttpStatus.UNPROCESSABLE_ENTITY) {
 
-        client.get()
-            .uri("/recommendation?productId=$productIdInvalid")
+            // Then
+            jsonPath("$.path").isEqualTo("/recommendation")
+            jsonPath("$.message").isEqualTo("Invalid productId: $invalidProductId")
+        }
+    }
+
+    private fun getAndVerifyRecommendations(
+        productId: Int,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit = {},
+    ) {
+        getAndVerifyRecommendations("?productId=$productId", expectedStatus, bodyAssertions)
+    }
+
+    private fun getAndVerifyRecommendations(
+        query: String,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit,
+    ) {
+        val bodyContentSpec = client.get()
+            .uri("/recommendation$query")
             .accept(APPLICATION_JSON)
             .exchange()
-            .expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+            .expectStatus().isEqualTo(expectedStatus)
             .expectHeader().contentType(APPLICATION_JSON)
             .expectBody()
-            .jsonPath("$.path").isEqualTo("/recommendation")
-            .jsonPath("$.message").isEqualTo("Invalid productId: $productIdInvalid")
+
+        bodyAssertions(bodyContentSpec)
+    }
+
+    private fun postAndVerifyRecommendation(
+        productId: Int,
+        recommendationId: Int,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit = {},
+    ) {
+        val recommendation = Recommendation(
+            productId = productId,
+            recommendationId = recommendationId,
+            author = "Author $recommendationId",
+            rate = recommendationId,
+            content = "Content $recommendationId",
+            serviceAddress = "some service address",
+        )
+
+        val bodyContentSpec = client.post()
+            .uri("/recommendation")
+            .body(recommendation.toMono(), Product::class.java)
+            .accept(APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isEqualTo(expectedStatus)
+            .expectHeader().contentType(APPLICATION_JSON)
+            .expectBody()
+
+        bodyAssertions(bodyContentSpec)
+    }
+
+    private fun deleteAndVerifyRecommendations(
+        productId: Int,
+        expectedStatus: HttpStatus,
+        bodyAssertions: WebTestClient.BodyContentSpec.() -> Unit = {}
+    ) {
+        val bodyContentSpec = client.delete()
+            .uri("/recommendation?productId=$productId")
+            .accept(APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isEqualTo(expectedStatus)
+            .expectBody()
+
+        bodyAssertions(bodyContentSpec)
     }
 }
