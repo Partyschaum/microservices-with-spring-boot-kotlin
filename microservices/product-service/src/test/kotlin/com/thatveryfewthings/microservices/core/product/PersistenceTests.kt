32de92f2f79eb3e3fb0d6f34dea5a8e573797087
Hauke Stange <hauke.stange@jimdo.com>
2021-12-01 14:10:24 +0100

    Refactor the product persistence to become reactive

--- microservices/product-service/src/test/kotlin/com/thatveryfewthings/microservices/core/product/PersistenceTests.kt
+++ microservices/product-service/src/test/kotlin/com/thatveryfewthings/microservices/core/product/PersistenceTests.kt
@@ -1,187 +1,149 @@
 package com.thatveryfewthings.microservices.core.product
 
-import com.mongodb.assertions.Assertions.assertFalse
-import com.mongodb.assertions.Assertions.assertTrue
 import com.thatveryfewthings.MongoDbTest
 import com.thatveryfewthings.microservices.core.product.persistence.ProductEntity
 import com.thatveryfewthings.microservices.core.product.persistence.ProductRepository
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.dao.DuplicateKeyException
 import org.springframework.dao.OptimisticLockingFailureException
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort.Direction.ASC
-import org.springframework.data.repository.findByIdOrNull
 import org.testcontainers.junit.jupiter.Testcontainers
+import reactor.test.StepVerifier
 
 @DataMongoTest(excludeAutoConfiguration = [EmbeddedMongoAutoConfiguration::class])
 @Testcontainers
 class PersistenceTests(
     @Autowired
-    private val repository: ProductRepository
+    private val repository: ProductRepository,
 ) : MongoDbTest() {
 
     @BeforeEach
     fun clearTable() {
-        repository.deleteAll()
+        repository.deleteAll().block()
     }
 
     @Test
     fun create() {
-        // Given
         val newEntity = ProductEntity(productId = 1, name = "some name", weight = 2)
-        repository.save(newEntity)
+        StepVerifier.create(repository.save(newEntity))
+            .expectNextMatches { newEntity.productId == it.productId }
+            .verifyComplete()
 
-        // When
-        val persistedEntity = repository.findByIdOrNull(newEntity.id)!!
+        StepVerifier.create(repository.findById(newEntity.id!!))
+            .expectProductsAreEqual(newEntity)
+            .verifyComplete()
 
-        // Then
-        assertEqualsProduct(newEntity, persistedEntity)
-        assertEquals(1, repository.count())
+        StepVerifier.create(repository.count())
+            .expectNext(1)
+            .verifyComplete()
     }
 
     @Test
     fun update() {
-        // Given
-        val persistedEntity = aPersistedProductEntity(productId = 2, name = "some name", weight = 3)
+        withPersistedProductEntity(productId = 2, name = "some name", weight = 3) { persistedEntity ->
 
-        // When
-        persistedEntity.name = "some other name"
-        repository.save(persistedEntity)
+            persistedEntity.name = "some other name"
 
-        // Then
-        assertEquals(1, persistedEntity.version)
-        assertEquals("some other name", persistedEntity.name)
+            StepVerifier.create(repository.save(persistedEntity))
+                .expectNextMatches { it.name == "some other name" }
+                .verifyComplete()
+
+            StepVerifier.create(repository.findById(persistedEntity.id!!))
+                .expectNextMatches { it.version == 1 && it.name == "some other name" }
+                .verifyComplete()
+        }
     }
 
     @Test
     fun delete() {
-        // Given
-        val persistedEntity = aPersistedProductEntity(productId = 3, name = "some name", weight = 4)
+        withPersistedProductEntity(productId = 3, name = "some name", weight = 4) { persistedEntity ->
 
-        // When
-        repository.delete(persistedEntity)
+            StepVerifier.create(repository.delete(persistedEntity))
+                .verifyComplete()
 
-        // Then
-        assertFalse(repository.existsById(persistedEntity.id!!))
+            StepVerifier.create(repository.existsById(persistedEntity.id!!))
+                .expectNext(false)
+                .verifyComplete()
+        }
     }
 
     @Test
     fun getByProductId() {
-        // Given
-        val persistedEntity = aPersistedProductEntity(productId = 4, name = "some different name", weight = 5)
-
-        // When
-        val foundEntity = repository.findByProductId(persistedEntity.productId)
-
-        // Then
-        assertNotNull(foundEntity)
-        assertEqualsProduct(persistedEntity, foundEntity!!)
+        withPersistedProductEntity(productId = 4, name = "some different name", weight = 5) { persistedEntity ->
+            StepVerifier.create(repository.findByProductId(persistedEntity.productId))
+                .expectProductsAreEqual(persistedEntity)
+                .verifyComplete()
+        }
     }
 
     @Test
     fun duplicateError() {
-        // Given
-        val persistedEntity = aPersistedProductEntity(productId = 5, name = "some name", weight = 6)
+        withPersistedProductEntity(productId = 5, name = "some name", weight = 6) { persistedEntity ->
 
-        // When
-        val newEntity = ProductEntity(
-            productId = persistedEntity.productId,
-            name = "some different name",
-            weight = 7
-        )
+            val newEntity = ProductEntity(
+                productId = persistedEntity.productId,
+                name = "some different name",
+                weight = 7
+            )
 
-        // Then
-        assertThrows<DuplicateKeyException> {
-            repository.save(newEntity)
+            StepVerifier.create(repository.save(newEntity))
+                .expectError(DuplicateKeyException::class.java)
+                .verify()
         }
     }
 
     @Test
     fun optimisticLockingError() {
-        // Given
-        val persistedEntity = aPersistedProductEntity(productId = 1, name = "some name", weight = 2)
+        withPersistedProductEntity(productId = 1, name = "some name", weight = 2) { persistedEntity ->
 
-        val foundEntity1 = repository.findByIdOrNull(persistedEntity.id)!!
-        val foundEntity2 = repository.findByIdOrNull(persistedEntity.id)!!
+            val foundEntity1 = repository.findById(persistedEntity.id!!).block()!!
+            val foundEntity2 = repository.findById(persistedEntity.id!!).block()!!
 
-        // When
-        foundEntity1.name = "some new name"
-        repository.save(foundEntity1)
+            foundEntity1.name = "some new name"
+            repository.save(foundEntity1).block()
 
-        // Then
-        assertThrows<OptimisticLockingFailureException> {
-            foundEntity2.name = "some completely new name"
-            repository.save(foundEntity2)
-        }
+            StepVerifier.create(repository.save(foundEntity2))
+                .expectError(OptimisticLockingFailureException::class.java)
+                .verify()
 
-        val updatedEntity = repository.findByIdOrNull(persistedEntity.id)!!
-        assertEquals(1, updatedEntity.version)
-        assertEquals("some new name", updatedEntity.name)
-    }
+            StepVerifier.create(repository.findById(persistedEntity.id!!))
+                .expectNextMatches {
+                    it.version == 1 && it.name == "some new name"
+                }
+                .verifyComplete()
 
-    @Test
-    fun paging() {
-        // Given
-        val newProducts = (1001..1010).map {
-            ProductEntity(it, "product $it", it)
-        }
-        repository.saveAll(newProducts)
-
-        // When
-        var nextPage: Pageable = PageRequest.of(0, 4, ASC, "productId")
-
-        // Then
-        nextPage = assertNextPage(nextPage) { page ->
-            assertEquals(listOf(1001, 1002, 1003, 1004), page.content.map { it.productId })
-            assertTrue(page.hasNext())
-        }
-
-        nextPage = assertNextPage(nextPage) { page ->
-            assertEquals(listOf(1005, 1006, 1007, 1008), page.content.map { it.productId })
-            assertTrue(page.hasNext())
-        }
-
-        assertNextPage(nextPage) { page ->
-            assertEquals(listOf(1009, 1010), page.content.map { it.productId })
-            assertFalse(page.hasNext())
         }
     }
 
-    private fun aPersistedProductEntity(productId: Int, name: String, weight: Int): ProductEntity {
+    private fun withPersistedProductEntity(
+        productId: Int,
+        name: String,
+        weight: Int,
+        onEntity: (entity: ProductEntity) -> Unit,
+    ) {
         val newEntity = ProductEntity(
             productId = productId,
             name = name,
             weight = weight,
         )
-        repository.save(newEntity)
-        return repository.findByIdOrNull(newEntity.id)!!
+        return repository.save(newEntity)
+            .doOnNext { repository.findById(it.id!!) }
+            .subscribe { onEntity(it) }
+            .dispose()
     }
 
-    private fun assertEqualsProduct(expectedEntity: ProductEntity, actualEntity: ProductEntity) {
-        assertEquals(expectedEntity.id, actualEntity.id)
-        assertEquals(expectedEntity.version, actualEntity.version)
-        assertEquals(expectedEntity.productId, actualEntity.productId)
-        assertEquals(expectedEntity.name, actualEntity.name)
-        assertEquals(expectedEntity.weight, actualEntity.weight)
+    private fun areProductsEqual(expectedEntity: ProductEntity, actualEntity: ProductEntity): Boolean {
+        return expectedEntity.id == actualEntity.id &&
+                expectedEntity.version == actualEntity.version &&
+                expectedEntity.productId == actualEntity.productId &&
+                expectedEntity.name == actualEntity.name &&
+                expectedEntity.weight == actualEntity.weight
     }
 
-    private fun assertNextPage(
-        nextPage: Pageable,
-        assertPage: (it: Page<ProductEntity>) -> Unit
-    ): Pageable {
-        val productPage = repository.findAll(nextPage)
-
-        assertPage(productPage)
-
-        return productPage.nextPageable()
+    private fun StepVerifier.Step<ProductEntity>.expectProductsAreEqual(expectedEntity: ProductEntity): StepVerifier.Step<ProductEntity> {
+        return expectNextMatches { areProductsEqual(expectedEntity, it) }
     }
 }
